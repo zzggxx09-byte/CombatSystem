0108e689f41da9036dbe41e89b16753ca8c0afc0
zzggxx09-byte <zzggxx09@gmail.com>
2026-07-10 05:59:59 +0300

    Update CombatSystem.java

--- src/main/java/com/example/combatsystem/CombatSystem.java
+++ src/main/java/com/example/combatsystem/CombatSystem.java
@@ -4,14 +4,11 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.relauncher.Side;
 import net.minecraftforge.common.MinecraftForge;
 
 @Mod(modid = "combatsystem", name = "Combat System", version = "1.0")
 public class CombatSystem {
-    
-    public static final String MODID = "combatsystem";
-    public static final String NAME = "Combat System";
-    public static final String VERSION = "1.0";
     
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
@@ -20,6 +17,12 @@ public class CombatSystem {
     
     @EventHandler
     public void init(FMLInitializationEvent event) {
+        // Серверна сторона
         MinecraftForge.EVENT_BUS.register(new CombatEventHandler());
+        
+        // Клієнтська сторона
+        if (event.getSide() == Side.CLIENT) {
+            MinecraftForge.EVENT_BUS.register(new CooldownIndicatorRenderer());
+        }
     }
 }
